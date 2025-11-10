a80b721fd19c554db7561b8bd339c40d1bccafbc
geniusjun <geniusjun4663@gmail.com>
2025-11-10 21:31:08 +0900

    Feat(MemberRepository): Spring Data Jpa를 활용할 jpa 상속 추상화 클래스 생성

--- src/main/kotlin/com/geniusjun/lotto/domain/member/Member.kt
+++ src/main/kotlin/com/geniusjun/lotto/domain/member/Member.kt
@@ -1,7 +1,12 @@
-package com.geniusjun.lotto.domain
+package com.geniusjun.lotto.domain.member
 
 import com.geniusjun.lotto.domain.common.BaseEntity
-import jakarta.persistence.*
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
 
 @Entity
 @Table(name = "members")
