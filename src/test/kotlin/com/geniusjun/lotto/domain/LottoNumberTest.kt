31cd920db78f2bb69f91d0de3e4291565f720931
geniusjun <geniusjun4663@gmail.com>
2025-11-11 17:28:04 +0900

    chore(LottoNumberTest): 파일 구조 수정

--- src/test/kotlin/com/geniusjun/lotto/domain/LottoNumberTest.kt
+++ src/test/kotlin/com/geniusjun/lotto/domain/LottoNumberTest.kt
@@ -1,5 +1,7 @@
-package com.geniusjun.lotto.domain.lotto
+package com.geniusjun.lotto.domain
 
+import com.geniusjun.lotto.domain.lotto.InvalidLottoException
+import com.geniusjun.lotto.domain.lotto.LottoNumber
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
@@ -11,7 +13,7 @@ class LottoNumberTest {
     @Test
     @DisplayName("번호가 1부터 45 사이면 정상적으로 생성된다")
     fun create_number_within_valid_range() {
-        val num = LottoNumber.of(10)
+        val num = LottoNumber.Companion.of(10)
         assertEquals(10, num.value)
     }
 
@@ -19,7 +21,7 @@ class LottoNumberTest {
     @DisplayName("번호가 1 미만 또는 45 초과이면 예외가 발생한다")
     fun create_number_out_of_range_should_throw_exception() {
         val ex = assertThrows<InvalidLottoException> {
-            LottoNumber.of(0)
+            LottoNumber.Companion.of(0)
         }
     }
 }
