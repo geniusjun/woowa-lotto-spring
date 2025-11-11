8f84bd90af74a0e3fdd1e932ab9f2868f0939cea
geniusjun <geniusjun4663@gmail.com>
2025-11-11 17:27:27 +0900

    Test(LottoNumberTest): 원시값 포장한 로또 숫자 테스트 구현

---
+++ src/test/kotlin/com/geniusjun/lotto/domain/LottoNumberTest.kt
@@ -0,0 +1,25 @@
+package com.geniusjun.lotto.domain.lotto
+
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import kotlin.test.assertEquals
+
+@DisplayName("LottoNumber 도메인 테스트")
+class LottoNumberTest {
+
+    @Test
+    @DisplayName("번호가 1부터 45 사이면 정상적으로 생성된다")
+    fun create_number_within_valid_range() {
+        val num = LottoNumber.of(10)
+        assertEquals(10, num.value)
+    }
+
+    @Test
+    @DisplayName("번호가 1 미만 또는 45 초과이면 예외가 발생한다")
+    fun create_number_out_of_range_should_throw_exception() {
+        val ex = assertThrows<InvalidLottoException> {
+            LottoNumber.of(0)
+        }
+    }
+}
