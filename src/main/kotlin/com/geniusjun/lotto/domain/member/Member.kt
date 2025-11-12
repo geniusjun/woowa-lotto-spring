024a939fd8f6fbe37e047cc031d00317b49e483a
geniusjun <geniusjun4663@gmail.com>
2025-11-12 13:35:51 +0900

    Feat(Member): 회원의 금액 증감 로직 추가

--- src/main/kotlin/com/geniusjun/lotto/domain/member/Member.kt
+++ src/main/kotlin/com/geniusjun/lotto/domain/member/Member.kt
@@ -1,12 +1,8 @@
 package com.geniusjun.lotto.domain.member
 
 import com.geniusjun.lotto.domain.common.BaseEntity
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
-import jakarta.persistence.Table
+import com.geniusjun.lotto.domain.member.exception.InvalidBalanceException
+import jakarta.persistence.*
 
 @Entity
 @Table(name = "members")
@@ -23,4 +19,21 @@ class Member(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null
+
+    fun decreaseBalance(amount: Long) {
+        if (amount <= 0) {
+            throw InvalidBalanceException("차감 금액은 0보다 커야 합니다. (입력값: $amount)")
+        }
+        if (balance < amount) {
+            throw InvalidBalanceException("잔액이 부족합니다. (현재 잔액: $balance, 필요 금액: $amount)")
+        }
+        balance -= amount
+    }
+
+    fun increaseBalance(amount: Long) {
+        if (amount < 0) {
+            throw InvalidBalanceException("보상 금액은 음수가 될 수 없습니다. (입력값: $amount)")
+        }
+        balance += amount
+    }
 }
