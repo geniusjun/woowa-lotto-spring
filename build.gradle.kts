239d04e26f2fd420ab7e2ce58ba2a66ca432f56f
geniusjun <geniusjun4663@gmail.com>
2025-11-12 22:54:52 +0900

    Infra(build.gradle.kts): 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,15 @@ dependencies {
 	// redis
 	implementation("org.springframework.boot:spring-boot-starter-data-redis")
 
+	// spring security & jwt
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+
+	// Read .env
+	implementation("me.paulschwarz:spring-dotenv:4.0.0")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
