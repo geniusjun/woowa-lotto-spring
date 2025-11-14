46e29083a253f403683dca8bfc2cea73a3048390
geniusjun <geniusjun4663@gmail.com>
2025-11-14 22:30:02 +0900

    Refactor(WinningNumbersRepository): 로또 당첨 번호 jpa 로직 수정

--- src/main/kotlin/com/geniusjun/lotto/domain/lotto/WinningNumbersRepository.kt
+++ src/main/kotlin/com/geniusjun/lotto/domain/lotto/WinningNumbersRepository.kt
@@ -3,6 +3,6 @@ package com.geniusjun.lotto.domain.lotto
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface WinningNumbersRepository : JpaRepository<WinningNumbersEntity, Long> {
-    fun findByRound(round: Long): WinningNumbersEntity?
-    fun findTop1ByOrderByRoundDesc(): WinningNumbersEntity?
+    fun findTop1ByOrderByUpdatedAtDesc(): WinningNumbersEntity?
 }
+
