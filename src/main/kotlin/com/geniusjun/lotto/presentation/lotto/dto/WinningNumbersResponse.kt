aeeffd03afba3d951570c40d7e17a8e4fcc62e57
geniusjun <geniusjun4663@gmail.com>
2025-11-14 22:29:28 +0900

    Refactor(WinningNumbersResponse): 로또 당첨 번호 응답형식 수정

--- src/main/kotlin/com/geniusjun/lotto/presentation/lotto/dto/WinningNumbersResponse.kt
+++ src/main/kotlin/com/geniusjun/lotto/presentation/lotto/dto/WinningNumbersResponse.kt
@@ -1,7 +1,6 @@
 package com.geniusjun.lotto.presentation.lotto.dto
 
 data class WinningNumbersResponse(
-    val round: Long,
     val mainNumbers: List<Int>,
-    val bonusNumber: Int?
+    val bonusNumber: Int
 )
